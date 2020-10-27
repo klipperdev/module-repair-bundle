3fbf072b7d29bb282e31258e8ff7b380c31e634e
François Pluchino <francois.pluchino@klipper.dev>
2020-10-27 14:54:33 +0100

    Add breakdown model

---
+++ Model/AbstractBreakdown.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * This file is part of the Klipper package.
+ *
+ * (c) François Pluchino <francois.pluchino@klipper.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Klipper\Module\RepairBundle\Model;
+
+use Klipper\Component\Model\Traits\LabelableTrait;
+use Klipper\Component\Model\Traits\OrganizationalRequiredTrait;
+use Klipper\Component\Model\Traits\TimestampableTrait;
+use Klipper\Component\Model\Traits\UserTrackableTrait;
+
+/**
+ * Breakdown model.
+ *
+ * @author François Pluchino <francois.pluchino@klipper.dev>
+ */
+abstract class AbstractBreakdown implements BreakdownInterface
+{
+    use LabelableTrait;
+    use OrganizationalRequiredTrait;
+    use TimestampableTrait;
+    use UserTrackableTrait;
+}
---
+++ Model/BreakdownInterface.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * This file is part of the Klipper package.
+ *
+ * (c) François Pluchino <francois.pluchino@klipper.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Klipper\Module\RepairBundle\Model;
+
+use Klipper\Component\Model\Traits\IdInterface;
+use Klipper\Component\Model\Traits\LabelableInterface;
+use Klipper\Component\Model\Traits\OrganizationalRequiredInterface;
+use Klipper\Component\Model\Traits\TimestampableInterface;
+use Klipper\Component\Model\Traits\UserTrackableInterface;
+
+/**
+ * Breakdown interface.
+ *
+ * @author François Pluchino <francois.pluchino@klipper.dev>
+ */
+interface BreakdownInterface extends
+    IdInterface,
+    LabelableInterface,
+    OrganizationalRequiredInterface,
+    TimestampableInterface,
+    UserTrackableInterface
+{
+}
