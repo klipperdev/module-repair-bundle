65b2edfeaded3287c07e0a542c28b8b8d4ce8de4
François Pluchino <francois.pluchino@klipper.dev>
2020-10-27 18:51:38 +0100

    Add repair history model

---
+++ Model/AbstractRepairHistory.php
@@ -0,0 +1,195 @@
+<?php
+
+/*
+ * This file is part of the Klipper package.
+ *
+ * (c) François Pluchino <francois.pluchino@klipper.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Klipper\Module\RepairBundle\Model;
+
+use Doctrine\ORM\Mapping as ORM;
+use JMS\Serializer\Annotation as Serializer;
+use Klipper\Component\DoctrineChoice\Model\ChoiceInterface;
+use Klipper\Component\DoctrineChoice\Validator\Constraints\EntityDoctrineChoice;
+use Klipper\Component\Model\Traits\OrganizationalRequiredTrait;
+use Klipper\Component\Model\Traits\TimestampableTrait;
+use Klipper\Component\Model\Traits\UserTrackableTrait;
+use Klipper\Module\DeviceBundle\Model\DeviceInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * Repair history model.
+ *
+ * @author François Pluchino <francois.pluchino@klipper.dev>
+ */
+abstract class AbstractRepairHistory implements RepairHistoryInterface
+{
+    use OrganizationalRequiredTrait;
+    use TimestampableTrait;
+    use UserTrackableTrait;
+
+    /**
+     * @ORM\ManyToOne(
+     *     targetEntity="Klipper\Module\RepairBundle\Model\RepairInterface",
+     *     fetch="EAGER"
+     * )
+     *
+     * @Assert\NotBlank
+     *
+     * @Serializer\Expose
+     */
+    protected ?RepairInterface $repair = null;
+
+    /**
+     * @ORM\Column(type="boolean")
+     *
+     * @Assert\Type(type="boolean")
+     *
+     * @Serializer\Expose
+     */
+    protected bool $public = false;
+
+    /**
+     * @ORM\Column(type="boolean")
+     *
+     * @Assert\Type(type="boolean")
+     *
+     * @Serializer\Expose
+     */
+    protected bool $swap = false;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Klipper\Component\DoctrineChoice\Model\ChoiceInterface", fetch="EAGER")
+     *
+     * @EntityDoctrineChoice("repair_status")
+     *
+     * @Serializer\Expose
+     */
+    protected ?ChoiceInterface $previousStatus = null;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Klipper\Component\DoctrineChoice\Model\ChoiceInterface", fetch="EAGER")
+     *
+     * @EntityDoctrineChoice("repair_status")
+     *
+     * @Assert\NotBlank
+     *
+     * @Serializer\Expose
+     */
+    protected ?ChoiceInterface $newStatus = null;
+
+    /**
+     * @ORM\ManyToOne(
+     *     targetEntity="Klipper\Module\DeviceBundle\Model\DeviceInterface",
+     *     fetch="EAGER"
+     * )
+     *
+     * @Serializer\Expose
+     */
+    protected ?DeviceInterface $previousDevice = null;
+
+    /**
+     * @ORM\ManyToOne(
+     *     targetEntity="Klipper\Module\DeviceBundle\Model\DeviceInterface",
+     *     fetch="EAGER"
+     * )
+     *
+     * @Serializer\Expose
+     */
+    protected ?DeviceInterface $newDevice = null;
+
+    public function setRepair(?RepairInterface $repair): self
+    {
+        $this->repair = $repair;
+
+        return $this;
+    }
+
+    public function getRepair(): ?RepairInterface
+    {
+        return $this->repair;
+    }
+
+    public function getRepairId()
+    {
+        return null !== $this->getRepair()
+            ? $this->getRepair()->getId()
+            : null;
+    }
+
+    public function setPublic(bool $public): self
+    {
+        $this->public = $public;
+
+        return $this;
+    }
+
+    public function isPublic(): bool
+    {
+        return $this->public;
+    }
+
+    public function setSwap(bool $swap): self
+    {
+        $this->swap = $swap;
+
+        return $this;
+    }
+
+    public function isSwap(): bool
+    {
+        return $this->swap;
+    }
+
+    public function setPreviousStatus(?ChoiceInterface $previousStatus): self
+    {
+        $this->previousStatus = $previousStatus;
+
+        return $this;
+    }
+
+    public function getPreviousStatus(): ?ChoiceInterface
+    {
+        return $this->previousStatus;
+    }
+
+    public function setNewStatus(?ChoiceInterface $newStatus): self
+    {
+        $this->newStatus = $newStatus;
+
+        return $this;
+    }
+
+    public function getNewStatus(): ?ChoiceInterface
+    {
+        return $this->newStatus;
+    }
+
+    public function setPreviousDevice(?DeviceInterface $previousDevice): self
+    {
+        $this->previousDevice = $previousDevice;
+
+        return $this;
+    }
+
+    public function getPreviousDevice(): ?DeviceInterface
+    {
+        return $this->previousDevice;
+    }
+
+    public function setNewDevice(?DeviceInterface $newDevice): self
+    {
+        $this->newDevice = $newDevice;
+
+        return $this;
+    }
+
+    public function getNewDevice(): ?DeviceInterface
+    {
+        return $this->newDevice;
+    }
+}
---
+++ Model/RepairHistoryInterface.php
@@ -0,0 +1,85 @@
+<?php
+
+/*
+ * This file is part of the Klipper package.
+ *
+ * (c) François Pluchino <francois.pluchino@klipper.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Klipper\Module\RepairBundle\Model;
+
+use Klipper\Component\DoctrineChoice\Model\ChoiceInterface;
+use Klipper\Component\Model\Traits\IdInterface;
+use Klipper\Component\Model\Traits\OrganizationalRequiredInterface;
+use Klipper\Component\Model\Traits\TimestampableInterface;
+use Klipper\Component\Model\Traits\UserTrackableInterface;
+use Klipper\Module\DeviceBundle\Model\DeviceInterface;
+
+/**
+ * Repair history interface.
+ *
+ * @author François Pluchino <francois.pluchino@klipper.dev>
+ */
+interface RepairHistoryInterface extends
+    IdInterface,
+    OrganizationalRequiredInterface,
+    TimestampableInterface,
+    UserTrackableInterface
+{
+    /**
+     * @return static
+     */
+    public function setRepair(?RepairInterface $repair);
+
+    public function getRepair(): ?RepairInterface;
+
+    /**
+     * @return null|int|string
+     */
+    public function getRepairId();
+
+    /**
+     * @return static
+     */
+    public function setPublic(bool $subcontractor);
+
+    public function isPublic(): bool;
+
+    /**
+     * @return static
+     */
+    public function setSwap(bool $swap);
+
+    public function isSwap(): bool;
+
+    /**
+     * @return static
+     */
+    public function setPreviousDevice(?DeviceInterface $previousDevice);
+
+    public function getPreviousDevice(): ?DeviceInterface;
+
+    /**
+     * @return static
+     */
+    public function setNewDevice(?DeviceInterface $previousDevice);
+
+    public function getNewDevice(): ?DeviceInterface;
+
+    /**
+     * @return static
+     */
+    public function setPreviousStatus(?ChoiceInterface $previousStatus);
+
+    public function getPreviousStatus(): ?ChoiceInterface;
+
+    /**
+     * @return static
+     */
+    public function setNewStatus(?ChoiceInterface $newStatus);
+
+    public function getNewStatus(): ?ChoiceInterface;
+}
