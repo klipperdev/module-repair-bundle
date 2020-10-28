64babc626c90dc01fe1c982b6634c886f274fb00
François Pluchino <francois.pluchino@klipper.dev>
2020-10-28 18:44:50 +0100

    Add coupon model

---
+++ Model/AbstractCoupon.php
@@ -0,0 +1,208 @@
+<?php
+
+/*
+ * This file is part of the Klipper package.
+ *
+ * (c) François Pluchino <francois.pluchino@klipper.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Klipper\Module\RepairBundle\Model;
+
+use Doctrine\ORM\Mapping as ORM;
+use JMS\Serializer\Annotation as Serializer;
+use Klipper\Component\DoctrineChoice\Model\ChoiceInterface;
+use Klipper\Component\DoctrineChoice\Validator\Constraints\EntityDoctrineChoice;
+use Klipper\Component\Model\Traits\OrganizationalRequiredTrait;
+use Klipper\Component\Model\Traits\TimestampableTrait;
+use Klipper\Module\PartnerBundle\Model\PartnerAddressInterface;
+use Klipper\Module\PartnerBundle\Model\Traits\AccountableRequiredTrait;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * Comment model.
+ *
+ * @author François Pluchino <francois.pluchino@klipper.dev>
+ */
+abstract class AbstractCoupon implements CouponInterface
+{
+    use AccountableRequiredTrait;
+    use OrganizationalRequiredTrait;
+    use TimestampableTrait;
+
+    /**
+     * @ORM\Column(type="string", length=80, nullable=true)
+     *
+     * @Assert\Type(type="string")
+     * @Assert\Length(min="0", max="80")
+     *
+     * @Serializer\Expose
+     */
+    protected ?string $reference = null;
+
+    /**
+     * @ORM\Column(type="string", length=128, nullable=true)
+     *
+     * @Assert\Type(type="string")
+     * @Assert\Length(min="0", max="128")
+     *
+     * @Serializer\Expose
+     */
+    protected ?string $internalContractReference = null;
+
+    /**
+     * @ORM\Column(type="string", length=128, nullable=true)
+     *
+     * @Assert\Type(type="string")
+     * @Assert\Length(min="0", max="128")
+     *
+     * @Serializer\Expose
+     */
+    protected ?string $customerReference = null;
+
+    /**
+     * @ORM\ManyToOne(
+     *     targetEntity="Klipper\Module\PartnerBundle\Model\PartnerAddressInterface",
+     *     fetch="EAGER"
+     * )
+     *
+     * @Serializer\Expose
+     */
+    protected ?PartnerAddressInterface $invoiceAddress = null;
+
+    /**
+     * @ORM\ManyToOne(
+     *     targetEntity="Klipper\Module\PartnerBundle\Model\PartnerAddressInterface",
+     *     fetch="EAGER"
+     * )
+     *
+     * @Serializer\Expose
+     */
+    protected ?PartnerAddressInterface $shippingAddress = null;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Klipper\Component\DoctrineChoice\Model\ChoiceInterface", fetch="EAGER")
+     *
+     * @EntityDoctrineChoice("coupon_status")
+     *
+     * @Serializer\Expose
+     */
+    protected ?ChoiceInterface $status = null;
+
+    /**
+     * @ORM\Column(type="datetime")
+     *
+     * @Assert\Type(type="datetime")
+     *
+     * @Serializer\Expose
+     */
+    protected ?\DateTimeInterface $validUntil = null;
+
+    /**
+     * @ORM\ManyToOne(
+     *     targetEntity="Klipper\Module\RepairBundle\Model\RepairInterface",
+     *     fetch="EAGER"
+     * )
+     *
+     * @Serializer\Expose
+     */
+    protected ?RepairInterface $usedByRepair = null;
+
+    public function setReference(?string $reference): self
+    {
+        $this->reference = $reference;
+
+        return $this;
+    }
+
+    public function getReference(): ?string
+    {
+        return $this->reference;
+    }
+
+    public function setInternalContractReference(?string $internalContractReference): self
+    {
+        $this->internalContractReference = $internalContractReference;
+
+        return $this;
+    }
+
+    public function getInternalContractReference(): ?string
+    {
+        return $this->internalContractReference;
+    }
+
+    public function setCustomerReference(?string $customerReference): self
+    {
+        $this->customerReference = $customerReference;
+
+        return $this;
+    }
+
+    public function getCustomerReference(): ?string
+    {
+        return $this->customerReference;
+    }
+
+    public function setInvoiceAddress(?PartnerAddressInterface $invoiceAddress): self
+    {
+        $this->invoiceAddress = $invoiceAddress;
+
+        return $this;
+    }
+
+    public function getInvoiceAddress(): ?PartnerAddressInterface
+    {
+        return $this->invoiceAddress;
+    }
+
+    public function setShippingAddress(?PartnerAddressInterface $shippingAddress): self
+    {
+        $this->shippingAddress = $shippingAddress;
+
+        return $this;
+    }
+
+    public function getShippingAddress(): ?PartnerAddressInterface
+    {
+        return $this->shippingAddress;
+    }
+
+    public function setStatus(?ChoiceInterface $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getStatus(): ?ChoiceInterface
+    {
+        return $this->status;
+    }
+
+    public function setValidUntil(?\DateTimeInterface $validUntil): self
+    {
+        $this->validUntil = $validUntil;
+
+        return $this;
+    }
+
+    public function getValidUntil(): ?\DateTimeInterface
+    {
+        return $this->validUntil;
+    }
+
+    public function setUsedByRepair(?RepairInterface $usedByRepair): self
+    {
+        $this->usedByRepair = $usedByRepair;
+
+        return $this;
+    }
+
+    public function getUsedByRepair(): ?RepairInterface
+    {
+        return $this->usedByRepair;
+    }
+}
---
+++ Model/CouponInterface.php
@@ -0,0 +1,87 @@
+<?php
+
+/*
+ * This file is part of the Klipper package.
+ *
+ * (c) François Pluchino <francois.pluchino@klipper.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Klipper\Module\RepairBundle\Model;
+
+use Klipper\Component\DoctrineChoice\Model\ChoiceInterface;
+use Klipper\Component\Model\Traits\IdInterface;
+use Klipper\Component\Model\Traits\OrganizationalRequiredInterface;
+use Klipper\Component\Model\Traits\TimestampableInterface;
+use Klipper\Module\PartnerBundle\Model\PartnerAddressInterface;
+use Klipper\Module\PartnerBundle\Model\Traits\AccountableRequiredInterface;
+
+/**
+ * Coupon interface.
+ *
+ * @author François Pluchino <francois.pluchino@klipper.dev>
+ */
+interface CouponInterface extends
+    AccountableRequiredInterface,
+    IdInterface,
+    OrganizationalRequiredInterface,
+    TimestampableInterface
+{
+    /**
+     * @return static
+     */
+    public function setReference(?string $reference);
+
+    public function getReference(): ?string;
+
+    /**
+     * @return static
+     */
+    public function setInternalContractReference(?string $internalContractReference);
+
+    public function getInternalContractReference(): ?string;
+
+    /**
+     * @return static
+     */
+    public function setCustomerReference(?string $customerReference);
+
+    public function getCustomerReference(): ?string;
+
+    /**
+     * @return static
+     */
+    public function setInvoiceAddress(?PartnerAddressInterface $invoiceAddress);
+
+    public function getInvoiceAddress(): ?PartnerAddressInterface;
+
+    /**
+     * @return static
+     */
+    public function setShippingAddress(?PartnerAddressInterface $shippingAddress);
+
+    public function getShippingAddress(): ?PartnerAddressInterface;
+
+    /**
+     * @return static
+     */
+    public function setStatus(?ChoiceInterface $status);
+
+    public function getStatus(): ?ChoiceInterface;
+
+    /**
+     * @return static
+     */
+    public function setValidUntil(?\DateTimeInterface $validUntil);
+
+    public function getValidUntil(): ?\DateTimeInterface;
+
+    /**
+     * @return static
+     */
+    public function setUsedByRepair(?RepairInterface $usedByRepair);
+
+    public function getUsedByRepair(): ?RepairInterface;
+}
