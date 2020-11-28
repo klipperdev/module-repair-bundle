fc21c4c166d7d65c66f30b37c91df68891ce5b16
François Pluchino <francois.pluchino@klipper.dev>
2020-11-28 15:13:49 +0100

    Add repair breakdown model

---
+++ Model/AbstractRepairBreakdown.php
@@ -0,0 +1,89 @@
+<?php
+
+/*
+ * This file is part of the Klipper package.
+ *
+ * (c) François Pluchino <francois.pluchino@klipper.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Klipper\Module\RepairBundle\Model;
+
+use Doctrine\ORM\Mapping as ORM;
+use JMS\Serializer\Annotation as Serializer;
+use Klipper\Component\Model\Traits\OrganizationalRequiredTrait;
+use Klipper\Component\Model\Traits\TimestampableTrait;
+use Klipper\Component\Model\Traits\UserTrackableTrait;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * Repair breakdown model.
+ *
+ * @author François Pluchino <francois.pluchino@klipper.dev>
+ *
+ * @Serializer\ExclusionPolicy("all")
+ */
+abstract class AbstractRepairBreakdown implements RepairBreakdownInterface
+{
+    use OrganizationalRequiredTrait;
+    use TimestampableTrait;
+    use UserTrackableTrait;
+
+    /**
+     * @ORM\ManyToOne(
+     *     targetEntity="Klipper\Module\RepairBundle\Model\RepairInterface",
+     *     fetch="EAGER"
+     * )
+     * @ORM\JoinColumn(onDelete="CASCADE", nullable=false)
+     *
+     * @Assert\NotBlank
+     *
+     * @Serializer\Expose
+     */
+    protected ?RepairInterface $repair = null;
+
+    /**
+     * @ORM\ManyToOne(
+     *     targetEntity="Klipper\Module\RepairBundle\Model\BreakdownInterface",
+     *     fetch="EAGER"
+     * )
+     *
+     * @Assert\NotBlank
+     *
+     * @Serializer\Expose
+     */
+    protected ?BreakdownInterface $breakdown = null;
+
+    public function setRepair(?RepairInterface $repair): self
+    {
+        $this->repair = $repair;
+
+        return $this;
+    }
+
+    public function getRepair(): ?RepairInterface
+    {
+        return $this->repair;
+    }
+
+    public function getRepairId()
+    {
+        return null !== $this->getRepair()
+            ? $this->getRepair()->getId()
+            : null;
+    }
+
+    public function setBreakdown(?BreakdownInterface $breakdown): self
+    {
+        $this->breakdown = $breakdown;
+
+        return $this;
+    }
+
+    public function getBreakdown(): ?BreakdownInterface
+    {
+        return $this->breakdown;
+    }
+}
---
+++ Model/RepairBreakdownInterface.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+ * This file is part of the Klipper package.
+ *
+ * (c) François Pluchino <francois.pluchino@klipper.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Klipper\Module\RepairBundle\Model;
+
+use Klipper\Component\Model\Traits\IdInterface;
+use Klipper\Component\Model\Traits\OrganizationalRequiredInterface;
+use Klipper\Component\Model\Traits\TimestampableInterface;
+use Klipper\Component\Model\Traits\UserTrackableInterface;
+
+/**
+ * Repair breakdown interface.
+ *
+ * @author François Pluchino <francois.pluchino@klipper.dev>
+ */
+interface RepairBreakdownInterface extends
+    IdInterface,
+    OrganizationalRequiredInterface,
+    TimestampableInterface,
+    UserTrackableInterface
+{
+    /**
+     * @return static
+     */
+    public function setRepair(?RepairInterface $repair);
+
+    public function getRepair(): ?RepairInterface;
+
+    /**
+     * @return null|int|string
+     */
+    public function getRepairId();
+
+    /**
+     * @return static
+     */
+    public function setBreakdown(?BreakdownInterface $breakdown);
+
+    public function getBreakdown(): ?BreakdownInterface;
+}
