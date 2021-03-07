73ac355cd95ab1bc2d16e4d15deff462e995e9cf
François Pluchino <francois.pluchino@klipper.dev>
2021-03-07 22:22:06 +0100

    Add repair impossible field on breakdown model

--- Model/AbstractBreakdown.php
+++ Model/AbstractBreakdown.php
@@ -11,11 +11,13 @@
 
 namespace Klipper\Module\RepairBundle\Model;
 
+use Doctrine\ORM\Mapping as ORM;
 use JMS\Serializer\Annotation as Serializer;
 use Klipper\Component\Model\Traits\LabelableTrait;
 use Klipper\Component\Model\Traits\OrganizationalRequiredTrait;
 use Klipper\Component\Model\Traits\TimestampableTrait;
 use Klipper\Component\Model\Traits\UserTrackableTrait;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * Breakdown model.
@@ -30,4 +32,25 @@ abstract class AbstractBreakdown implements BreakdownInterface
     use OrganizationalRequiredTrait;
     use TimestampableTrait;
     use UserTrackableTrait;
+
+    /**
+     * @ORM\Column(type="boolean")
+     *
+     * @Assert\Type(type="boolean")
+     *
+     * @Serializer\Expose
+     */
+    protected bool $repairImpossible = false;
+
+    public function setRepairImpossible(bool $repairImpossible): self
+    {
+        $this->repairImpossible = $repairImpossible;
+
+        return $this;
+    }
+
+    public function isRepairImpossible(): bool
+    {
+        return $this->repairImpossible;
+    }
 }
